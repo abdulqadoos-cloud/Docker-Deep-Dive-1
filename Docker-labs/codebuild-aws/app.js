ce72959008599836a5390ff2302cf41698f471b0
abdulqadoos <abdulqadoos.cloud@gmail.com>
2026-06-20 15:05:26 +0500

    "testing"

--- Docker-labs/codebuild-aws/app.js
+++ Docker-labs/codebuild-aws/app.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 8080;
 
 app.get('/', (req, res) => {
-    res.send('Hello World from AWS CodeBuild!');
+    res.send('Hello World from AWS CodeBuild! testing');
 });
 
 app.listen(PORT, () => {
